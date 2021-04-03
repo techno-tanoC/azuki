6fd28fa4f250c44142bc168f2fcd56aad45e3d7c
techno-tanoC <develop.tanoc@gmail.com>
2021-04-04 00:47:58 +0900

    add gin

--- server/main.go
+++ server/main.go
@@ -1,4 +1,19 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
+	r := gin.Default()
+
+	r.GET("/downloads", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "ok",
+		})
+	})
+
+	r.Run()
 }
