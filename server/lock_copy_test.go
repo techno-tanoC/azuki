81df264cb7692aa1d4bdc73a3ceac18dbf250971
techno-tanoC <develop.tanoc@gmail.com>
2021-04-03 14:19:10 +0900

    rewrite LockCopy tests

--- server/lock_copy_test.go
+++ server/lock_copy_test.go
@@ -1,23 +1,72 @@
 package main
 
 import (
+	"bytes"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
 )
 
-func TestBuildFreshName(t *testing.T) {
-	lc := NewLockCopy(".", 1000, 1000)
+func TestCopyContents(t *testing.T) {
+	dir, err := ioutil.TempDir("", "azuki_*")
+	if err != nil {
+		t.Fatalf("TestCopyContents error creating temporary dir: %v", err)
+	}
+	defer os.RemoveAll(dir)
 
-	fresh := lc.buildFreshName("go", "mod")
-	diff := cmp.Diff(fresh, "go(1).mod")
-	if diff != "" {
-		t.Errorf("TestBuildFreshName: (-got +want)\n%s", diff)
+	lc := NewLockCopy(dir, 1000, 1000)
+	data := "ハローワールド"
+
+	buf := bytes.NewBuffer([]byte(data))
+	lc.Copy(buf, "azuki", "txt")
+
+	// Read copied file
+	contents, err := ioutil.ReadFile(filepath.Join(dir, "azuki.txt"))
+	if err != nil {
+		t.Fatalf("TestCopyContents error reading file: %v", err)
 	}
 
-	fresh = lc.buildFreshName("unknown", "mod")
-	diff = cmp.Diff(fresh, "unknown.mod")
+	diff := cmp.Diff(string(contents), data)
 	if diff != "" {
-		t.Errorf("TestBuildFreshName2: (-got +want)\n%s", diff)
+		t.Errorf("\n%s", diff)
+	}
+}
+
+func TestCopyName(t *testing.T) {
+	dir, err := ioutil.TempDir("", "azuki_*")
+	if err != nil {
+		t.Fatalf("TestCopyName: %v", err)
+	}
+	defer os.RemoveAll(dir)
+
+	lc := NewLockCopy(dir, 1000, 1000)
+
+	buf := bytes.NewBuffer([]byte(""))
+	lc.Copy(buf, "azuki", "txt")
+	lc.Copy(buf, "azuki", "txt")
+	lc.Copy(buf, "azuki", "txt")
+
+	// List copied files
+	fileInfos, err := ioutil.ReadDir(dir)
+	if err != nil {
+		t.Fatalf("TestCopyName: %v", err)
+	}
+
+	var names []string
+	for _, name := range fileInfos {
+		names = append(names, name.Name())
+	}
+
+	expectedNames := []string{"azuki.txt", "azuki(1).txt", "azuki(2).txt"}
+	opt := cmpopts.SortSlices(func(i, j string) bool {
+		return i < j
+	})
+	diff := cmp.Diff(names, expectedNames, opt)
+	if diff != "" {
+		t.Errorf("\n%s", diff)
 	}
 }
